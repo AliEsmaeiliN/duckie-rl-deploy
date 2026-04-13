d55c7cd9c6703cc0d9205bcf62e8857d4d83c3c9
AliEsmaeiliN <ali.n.esmaeilinasab@gmail.com>
2026-04-13 12:43:07 +0200

    adding the debug method

--- packages/solution.py
+++ packages/solution.py
@@ -14,6 +14,8 @@ class RLNode:
         rospy.init_node('rl_agent_node')
         self.veh = os.environ.get('VEHICLE_NAME', 'duckie1nav')
 
+        self.debug_mode = os.environ.get("DEBUG_MODE", "false").lower() == "true"
+
         repo_path = os.environ.get("DT_REPO_PATH", "/solution")
         model_full_path = os.path.join(repo_path, f"assets/models/{algo}_Final.cleanrl_model")
         
@@ -25,24 +27,31 @@ class RLNode:
         self.wheel_pub = rospy.Publisher(f"/{self.veh}/wheels_driver_node/wheels_cmd", WheelsCmdStamped, queue_size=1)
         self.sub = rospy.Subscriber(f"/{self.veh}/camera_node/image/compressed", CompressedImage, self.callback, queue_size=1, buff_size=2**24)
         
-        rospy.loginfo("Node started. Listening for camera frames...")
+        rospy.loginfo(f"Node started for {self.veh}. Mode: {'DEBUG' if self.debug_mode else 'INFERENCE'}")
         rospy.on_shutdown(self.emergency_stop)
 
     def callback(self, msg):
         np_arr = np.frombuffer(msg.data, np.uint8)
         obs = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-        run_remote_debug(self.agent, self, obs)
+
+        if self.debug_mode:
+            run_remote_debug(self.agent, self, obs)
+        else:
+            action = self.agent.get_action(obs)
+            wheel_cmds = self.agent.postprocess_kinematics(action)
+            self.write("wheels", wheel_cmds)
 
     def write(self, topic, data):
         if topic == 'wheels':
-            pass
+            cmd_msg = WheelsCmdStamped()
+            cmd_msg.header.stamp = rospy.Time.now()
+            cmd_msg.vel_left = data[0]
+            cmd_msg.vel_right = data[1]
+            self.wheel_pub.publish(cmd_msg)
             
     def emergency_stop(self):
         rospy.loginfo("Shutting down... sending stop command to wheels.")
-        stop_msg = WheelsCmdStamped()
-        stop_msg.vel_left = 0.0
-        stop_msg.vel_right = 0.0
-        self.wheel_pub.publish(stop_msg)
+        self.write("wheels", [0.0, 0.0])
 
 if __name__ == '__main__':
     algo_arg = "sac"
